c8ee4d1200979fd84bf7e0891b1c2652a78cff83
AndreaPanzeriDev <andpanz@libero.it>
2022-12-19 13:08:38 +0100

    accessori class

---
+++ classi/accessori.php
@@ -0,0 +1,18 @@
+<?php
+
+class Accessori{
+    public $materiale;
+    public $dimensioni;
+
+
+    public function __construct(
+        String $materiale,
+        String $dimensioni,
+    )
+    {
+        $this->materiale= $materiale;
+        $this->dimensioni= $dimensioni;
+    }
+}
+
+?>
