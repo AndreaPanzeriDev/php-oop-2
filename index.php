032e65f11cc70a7492e3f850b0a670d2b716cd26
AndreaPanzerDev <andpanz@libero.it>
2022-12-19 23:27:07 +0100

    finito

--- index.php
+++ index.php
@@ -13,10 +13,10 @@ $categorie = [
 
 
 $prodotto = [
-    new Cibo('https://picsum.photos/200/300?random=1', 'Royal Canin', 43.99, $categorie['cane'], 545, 'pollo'),
-    new Cibo('https://picsum.photos/200/300?random=2', 'Almo nature', 43.99, $categorie['cane'], 600, 'Grano'),
-    new Accessori('https://picsum.photos/200/300?random=3', 'Voliera Ferplast Bella Casa', 184.99, $categorie['uccello'], 'Legno', 'L83 x P 67 x H153cm'),
-    new Giocattoli('https://picsum.photos/200/300?random=4', 'Topini di peluche', 9.99, $categorie['uccello'], 'morbido ed intrattenente', '8.5 x 10')
+    new Cibo('https://picsum.photos/800/1200', 'Royal Canin', 43.99, $categorie['cane'], 545, 'pollo'),
+    new Cibo('https://picsum.photos/800/1200', 'Almo nature', 43.99, $categorie['cane'], 600, 'Grano'),
+    new Accessori('https://picsum.photos/800/1200', 'Voliera Ferplast Bella Casa', 184.99, $categorie['uccello'], 'Legno', 'L83 x P 67 x H153cm'),
+    new Giocattoli('https://picsum.photos/800/1200', 'Topini di peluche', 9.99, $categorie['gatto'], 'morbido ed intrattenente', '8.5 x 10')
 
 
 ];
@@ -32,6 +32,7 @@ $prodotto = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body>
@@ -46,7 +47,23 @@ $prodotto = [
             <img src="<?php echo $elem -> immagine ?>" alt="immagine" style="max-width:100%">
                 <div class="card-body">
                     <p class="card-text fw-bold fs-5"><?php echo $elem -> nome?></p>
-                    
+                    <p class="card-text"><?php echo ($elem -> category -> icon . ' ' . $elem -> category -> nome); ?></p>
+                    <p><?php echo ('Prezzo: $' . $elem->prezzo)?></p>
+                    <!--CIBO-->
+                    <?php if(get_class($elem) == "Cibo"){?>
+                        <p><?php echo ('Peso netto: ' . $elem->peso_netto . 'g');?></p> 
+                        <p><?php echo ('Ingredienti: ' . $elem->ingredienti)?></p> 
+                    <?php }?>
+                    <!--ACCESSORI-->
+                    <?php if(get_class($elem) == "Accessori"){?>
+                        <p><?php echo ('Materiale: ' . $elem->materiale);?></p> 
+                        <p><?php echo ('Dimensioni: ' . $elem->dimensioni)?></p> 
+                    <?php }?>
+                    <!--GIOCATTOLI-->
+                    <?php if(get_class($elem) == "Giocattoli"){?>
+                        <p><?php echo ('Caratteristiche: ' . $elem->caratteristiche);?></p> 
+                        <p><?php echo ('Dimensioni: ' . $elem->dimensioni . 'cm')?></p> 
+                    <?php }?>
                 </div>
             </div>
         <?php } ?>
