c4fcf6a9ad44ad0c929e44dd2b2a96d95351030f
AndreaPanzeriDev <andpanz@libero.it>
2022-12-19 13:00:01 +0100

    category

---
+++ classi/category.php
@@ -0,0 +1,17 @@
+<?php
+
+class Categoria
+{
+    public $nome;
+    public $icon;
+
+    public function __construct(
+        String $nome,
+        String $icon)
+    {
+        $this->nome= $nome;
+        $this->icon=$icon;
+    }
+}
+
+?>
